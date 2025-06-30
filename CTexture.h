a3c17d7e3069800682f495fa50d16d461f37ec54
oljindudol <82567430+oljindudol@users.noreply.github.com>
2025-06-30 18:04:00 +0900

    텍스쳐 유니크ptr사용하려고 생성자소멸자public

--- CTexture.h
+++ CTexture.h
@@ -3,30 +3,30 @@
 
 
 class CTexture :
-    public CAsset
+	public CAsset
 {
 private:
-    HBITMAP     m_hBit;
-    HDC         m_hDC;
-    BITMAP      m_Info;
+	HBITMAP     m_hBit;
+	HDC         m_hDC;
+	BITMAP      m_Info;
 
 public:
-    HDC GetDC() { return m_hDC; }
-    HBITMAP GetBitmap() { return m_hBit; }
-    UINT GetWidth() { return m_Info.bmWidth; }
-    UINT GetHeight() { return m_Info.bmHeight; }
+	HDC GetDC() { return m_hDC; }
+	HBITMAP GetBitmap() { return m_hBit; }
+	UINT GetWidth() { return m_Info.bmWidth; }
+	UINT GetHeight() { return m_Info.bmHeight; }
+	CTexture();
+	~CTexture();
 
 private:
-    virtual bool Load(const wstring& _strFilePath) override;
-    virtual bool Load_r(const wstring& _strFilePath);
-    virtual bool Load_rotated(const wstring& _strFilePath, float _rad);
+	virtual bool Load(const wstring& _strFilePath) override;
+	virtual bool Load_r(const wstring& _strFilePath);
+	virtual bool Load_rotated(const wstring& _strFilePath, float _rad);
 
-    void Create(UINT _Width, UINT Height);
+	void Create(UINT _Width, UINT Height);
 
 private:
-    CTexture();
-    ~CTexture();
 
-    friend class CAssetMgr;
+	friend class CAssetMgr;
 };
 
